7d5c84dc4f264aa70274edcee7971a3d43f6cd45
tpoteet <156221280+tpoteet@users.noreply.github.com>
2024-01-10 11:51:32 -0500

    Update config.ts

--- config.ts
+++ config.ts
@@ -1,9 +1,9 @@
 import { Config } from "./src/config";
 
 export const defaultConfig: Config = {
-  url: "https://www.builder.io/c/docs/developers",
-  match: "https://www.builder.io/c/docs/**",
-  maxPagesToCrawl: 50,
+  url: "https://www.support.powerofpublicspeaking.com",
+  match: "https://www.support.powerofpublicspeaking.com/post/**",
+  maxPagesToCrawl: 200,
   outputFileName: "output.json",
   maxTokens: 2000000,
 };
